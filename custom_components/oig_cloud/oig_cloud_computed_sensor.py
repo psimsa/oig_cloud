040ab7b0019370a3e0142807c9ba5c9d32645cfd
psimsa <pavel@simsa.cz>
2023-07-20 17:42:08 +0200

    Refactor (#44)

--- custom_components/oig_cloud/oig_cloud_computed_sensor.py
+++ custom_components/oig_cloud/oig_cloud_computed_sensor.py
@@ -25,7 +25,6 @@ _LANGS = {
 
 
 class OigCloudComputedSensor(OigCloudSensor):
-
     @property
     def state(self):
         _LOGGER.debug(f"Getting state for {self.entity_id}")
@@ -52,62 +51,68 @@ class OigCloudComputedSensor(OigCloudSensor):
             return float(pv_data["dc_in"]["fv_p1"] + pv_data["dc_in"]["fv_p2"])
 
         if self._node_id == "boiler" or self._sensor_type == "boiler_current_w":
-            if len(pv_data["boiler"]) > 0 and pv_data["boiler"]["p"] is not None:
-                # Spotreba bojleru
-                if (
-                        self._sensor_type == "boiler_current_w"
-                        and pv_data["boiler"]["p"] > 0
-                        and (
-                        pv_data["ac_in"]["aci_wr"]
-                        + pv_data["ac_in"]["aci_ws"]
-                        + pv_data["ac_in"]["aci_wt"]
-                )
-                        < 0
-                ):
-                    return float(
-                        pv_data["boiler"]["p"]
-                        + (
-                                pv_data["ac_in"]["aci_wr"]
-                                + pv_data["ac_in"]["aci_ws"]
-                                + pv_data["ac_in"]["aci_wt"]
-                        )
-                    )
-                elif self._sensor_type == "boiler_current_w":
-                    return float(pv_data["boiler"]["p"])
-            else:
-                return None
+            return self._get_boiler_consumption(pv_data)
 
         # Spotreba CBB
         if self._sensor_type == "cbb_consumption_w":
-            boiler_p = 0
+            return self._get_cbb_consumption(pv_data)
+
+        return None
+
+    def _get_cbb_consumption(self, pv_data) -> float:
+        boiler_p = 0
+        if (
+            len(pv_data["boiler"]) > 0
+            and pv_data["boiler"]["p"] is not None
+            and pv_data["boiler"]["p"] > 0
+        ):
+            boiler_p = pv_data["boiler"]["p"]
+        return float(
+            # Výkon FVE
+            (pv_data["dc_in"]["fv_p1"] + pv_data["dc_in"]["fv_p2"])
+            -
+            # Spotřeba bojleru
+            boiler_p
+            -
+            # Spotřeba zátěž
+            pv_data["ac_out"]["aco_p"]
+            +
+            # Odběr ze sítě
+            (
+                pv_data["ac_in"]["aci_wr"]
+                + pv_data["ac_in"]["aci_ws"]
+                + pv_data["ac_in"]["aci_wt"]
+            )
+            +
+            # Nabíjení/vybíjení baterie
+            (pv_data["batt"]["bat_i"] * pv_data["batt"]["bat_v"] * -1)
+        )
+
+    def _get_boiler_consumption(self, pv_data):
+        if len(pv_data["boiler"]) > 0 and pv_data["boiler"]["p"] is not None:
+            # Spotreba bojleru
             if (
-                    len(pv_data["boiler"]) > 0
-                    and pv_data["boiler"]["p"] is not None
-                    and pv_data["boiler"]["p"] > 0
+                self._sensor_type == "boiler_current_w"
+                and pv_data["boiler"]["p"] > 0
+                and (
+                    pv_data["ac_in"]["aci_wr"]
+                    + pv_data["ac_in"]["aci_ws"]
+                    + pv_data["ac_in"]["aci_wt"]
+                )
+                < 0
             ):
-                boiler_p = pv_data["boiler"]["p"]
-            return float(
-                # Výkon FVE
-                (pv_data["dc_in"]["fv_p1"] + pv_data["dc_in"]["fv_p2"])
-                -
-                # Spotřeba bojleru
-                boiler_p
-                -
-                # Spotřeba zátěž
-                pv_data["ac_out"]["aco_p"]
-                +
-                # Odběr ze sítě
-                (
+                return float(
+                    pv_data["boiler"]["p"]
+                    + (
                         pv_data["ac_in"]["aci_wr"]
                         + pv_data["ac_in"]["aci_ws"]
                         + pv_data["ac_in"]["aci_wt"]
+                    )
                 )
-                +
-                # Nabíjení/vybíjení baterie
-                (pv_data["batt"]["bat_i"] * pv_data["batt"]["bat_v"] * -1)
-            )
-
-        return None
+            elif self._sensor_type == "boiler_current_w":
+                return float(pv_data["boiler"]["p"])
+        else:
+            return None
 
     async def async_update(self):
         # Request the coordinator to fetch new data and update the entity's state
