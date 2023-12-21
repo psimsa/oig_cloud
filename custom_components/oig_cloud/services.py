3177ec3f14aa42f3cc7e66e491d77fe60b671646
Horak <137921218+Muriel2Horak@users.noreply.github.com>
2023-12-21 16:17:52 +0100

    FIX definice služeb

--- custom_components/oig_cloud/services.py
+++ custom_components/oig_cloud/services.py
@@ -16,7 +16,6 @@ MODES = {
 
 GRID_DELIVERY = {"Vypnuto / Off": 0, "Zapnuto / On": 1, "S omezením / Limited": 2}
 
-
 BOILER_MODE = {"CBB": 0, "Manual": 1}
 
 FORMAT_BATTERY = {"Nenabíjet": 0, "Nabíjet": 1}
@@ -24,7 +23,6 @@ FORMAT_BATTERY = {"Nenabíjet": 0, "Nabíjet": 1}
 SSR_MODE = {"Vypnuto": 0, "Zapnuto": 1}
 
 tracer = trace.get_tracer(__name__)
-
 
 async def async_setup_entry_services(hass: HomeAssistant, entry: ConfigEntry) -> None:
     async def async_set_box_mode(call):
