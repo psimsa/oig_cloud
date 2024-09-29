458fbfe6adf01dc496c2da6e5abf4b2522291fe2
Pavel Simsa <pavel@simsa.cz>
2024-09-29 09:58:29 +0200

    Separate sensor files

---
+++ custom_components/oig_cloud/sensors/SENSOR_TYPES_BATT.py
@@ -0,0 +1,26 @@
+from homeassistant.components.sensor import SensorDeviceClass, SensorStateClass
+
+
+from typing import Dict
+
+
+SENSOR_TYPES_BATT: Dict[str, Dict[str, str | SensorDeviceClass | SensorStateClass]] = {
+    "batt_bat_c": {
+        "name": "Battery Percent",
+        "name_cs": "Nabití baterie (procenta, live)",
+        "device_class": SensorDeviceClass.BATTERY,
+        "unit_of_measurement": "%",
+        "node_id": "actual",
+        "node_key": "bat_c",
+        "state_class": SensorStateClass.MEASUREMENT,
+    },
+    "batt_batt_comp_p": {
+        "name": "Battery Power",
+        "name_cs": "Výkon baterie (live)",
+        "device_class": SensorDeviceClass.POWER,
+        "unit_of_measurement": "W",
+        "node_id": "actual",
+        "node_key": "bat_p",
+        "state_class": SensorStateClass.MEASUREMENT,
+    },
+}
