427841de55d216f09387f96c99a1c8157d972918
Muriel2Horak <137921218+Muriel2Horak@users.noreply.github.com>
2024-10-01 09:23:07 +0200

    New senzors. Split battery power to separate values. (#73)
    Co-authored-by: Pavel Simsa <pavel@simsa.cz>

--- custom_components/oig_cloud/sensors/SENSOR_TYPES_BATT.py
+++ custom_components/oig_cloud/sensors/SENSOR_TYPES_BATT.py
@@ -23,4 +23,22 @@ SENSOR_TYPES_BATT: Dict[str, Dict[str, str | SensorDeviceClass | SensorStateClas
         "node_key": "bat_p",
         "state_class": SensorStateClass.MEASUREMENT,
     },
+    "batt_batt_comp_p_charge": {
+        "name": "Battery Charge Power",
+        "name_cs": "Výkon baterie - nabíjení (live)",
+        "device_class": SensorDeviceClass.POWER,
+        "unit_of_measurement": "W",
+        "node_id": None,
+        "node_key": None,
+        "state_class": SensorStateClass.MEASUREMENT,
+    },
+    "batt_batt_comp_p_discharge": {
+        "name": "Battery Discharge Power",
+        "name_cs": "Výkon baterie - vybíjení (live)",
+        "device_class": SensorDeviceClass.POWER,
+        "unit_of_measurement": "W",
+        "node_id": None,
+        "node_key": None,
+        "state_class": SensorStateClass.MEASUREMENT,
+    },    
 }
